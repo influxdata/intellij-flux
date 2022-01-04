45c61848183ccbb4046b6ef311928b26a3572ccf
GitHub Action <action@github.com>
2022-01-04 16:59:09 +0000

    Template cleanup

--- src/main/kotlin/com/github/onelson/intellijflux/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/onelson/intellijflux/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.onelson.intellijflux.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.onelson.intellijflux.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/onelson/intellijflux/services/MyProjectService.kt
+++ src/main/kotlin/com/github/onelson/intellijflux/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.onelson.intellijflux.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.onelson.intellijflux.MyBundle
 
 class MyProjectService(project: Project) {
 
