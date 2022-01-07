4dacff3f04fc67a84afd5ad8696ada99bc3c5e75
Owen Nelson <onelson@gmail.com>
2022-01-06 23:16:33 -0800

    refactor: switch to ballerina (#6)
    The lsp (gtache) plugin we based ours on crashed often when a flux source
    was invalid (failing type checks) and then, while in that state, the editor
    asks the LSP for completion or signature info.
    
    This diff switches us to depend on the "ballerina" LSP library which is
    effectively a fork of the original plugin we tried to use.
    
    "Ballerina" still has issues in similar situations, but is a little more
    recoverable. If the editor gets in a bad state and stops offering
    completions, closing and opening the buffer again will usually get it
    going again.
    
    While code reformatting didn't work at all with the gtache plugin, it
    ALSO doesn't work here with ballerina. It appears the support is there,
    but the config (in plugin.xml) may be outdated for the current intellij
    platform.
    
    ---
    
    N.b. the instructions for configuring the lib in the current master
    branch README are incorrect. I had to refer to the README from the
    0.94.2 tag to get this all wired up.

--- src/main/kotlin/com/influxdata/intellijflux/FluxPreloadingActivity.kt
+++ src/main/kotlin/com/influxdata/intellijflux/FluxPreloadingActivity.kt
@@ -1,20 +1,19 @@
 package com.influxdata.intellijflux
 
-import com.github.gtache.lsp.client.languageserver.serverdefinition.ExeLanguageServerDefinition
-import com.github.gtache.lsp.client.languageserver.serverdefinition.LanguageServerDefinition
 import com.intellij.openapi.application.PreloadingActivity
 import com.intellij.openapi.progress.ProgressIndicator
+import org.wso2.lsp4intellij.IntellijLanguageClient
+import org.wso2.lsp4intellij.client.languageserver.serverdefinition.RawCommandServerDefinition
 
 class FluxPreloadingActivity : PreloadingActivity() {
     override fun preload(indicator: ProgressIndicator) {
-        LanguageServerDefinition.register(
-            ExeLanguageServerDefinition(
+        IntellijLanguageClient.addServerDefinition(
+            RawCommandServerDefinition(
                 "flux",
                 // FIXME: read from settings instead of hard-coding (requiring the bin to be in PATH).
                 //  A wrinkle is we'd need a way to re-register the server
                 //  definition when the settings change.
-                "flux-lsp",
-                arrayOf()
+                arrayOf("flux-lsp", "--log-file", ".flux-lsp.log"),
             )
         )
     }
